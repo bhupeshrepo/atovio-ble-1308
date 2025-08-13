8d926772b26483da980b184de953204429e49f23
beingbhupesh <beingbhupesh.385@gmail.com>
2025-08-13 11:53:58 +0530

    all commit

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+
+const CACHE_NAME = 'atovio-ble-cache-v1';
+const urlsToCache = [
+  '/',
+  'index.html',
+  'manifest.json',
+  'service-worker.js',
+  'icons/icon-192.jpg',
+  'icons/icon-512.jpg'
+];
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        return response || fetch(event.request);
+      })
+  );
+});
